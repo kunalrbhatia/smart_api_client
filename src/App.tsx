f9a487d7761f12117db030147340275b156451fe
kunalrbhatia <kunal.bhatia@hotmail.com>
2022-08-12 18:26:34 +0530

    added websocket and received data

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
+import { Paper } from '@mui/material';
+import * as _ from 'lodash';
 function App() {
+  const [data, setData] = useState<string>('');
+  const socket: WebSocket = new WebSocket('ws://localhost:5000');
+  socket.addEventListener('open', (evt: any) => {});
+  socket.addEventListener('message', (evt: any) => {
+    setData(_.get(JSON.parse(evt.data)[0], 'tvalue', 'no value') || 'no value');
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Paper elevation={3}>
+        <div>{data}</div>
+      </Paper>
     </div>
   );
 }
