e630c822c1f317fdd12cff395e079516f9e82d5c
Kunal Bhatia <kunal.bhaia@hotmail.com>
2023-12-16 00:44:49 +0530

    fix base url

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,3 @@
-export const BASE_URL =
-  process.env.NODE_ENV === 'production'
-    ? 'https://orb-algo-server-image-ckdbs7u4gq-uc.a.run.app'
-    : 'http://localhost:6000';
+export const BASE_URL = 'https://orb-algo-server-image-ckdbs7u4gq-uc.a.run.app';
 export const ORB_ALGO = `${BASE_URL}/orb`;
 export const GET_STOCK = `${BASE_URL}/stock`;
